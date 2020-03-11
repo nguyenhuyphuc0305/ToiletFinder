bacccf9d8a67855519fe1fac220bdc7cbb2ebed8
huyphuc0305 <huyphuc.nguyen0305@gmail.com>
2020-03-10 21:48:59 -0400

    add clickable marker and flickr api call

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
 const express = require("express");
 const app = express();
 const port = 8080;
+const cors = require("cors")
 
+app.use(cors());
 app.use(express.static("public"));
+
+app.use(cors({
+    'allowedHeaders': ['sessionId', 'Content-Type'],
+    'exposedHeaders': ['sessionId'],
+    'origin': '*',
+    'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    'preflightContinue': false
+}));  
+
 app.use(require("./routes"));
 app.listen(port, () => console.log(`Listening on port ${port}!`));
--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ const path = require("path")
 const request = require("request")
 
 const router = express.Router()
+const flickrKey = require("../flickr_API.json")
 
 const mysql = require('mysql');
 const db = mysql.createConnection({
@@ -12,26 +13,34 @@ const db = mysql.createConnection({
 	database: 'toiletdb'
 });
 
-db.connect((err) => {
-	if (err) throw err;
-	console.log('Connected to tolietdb')
-});
+// db.connect((err) => {
+// 	if (err) throw err;
+// 	console.log('Connected to tolietdb')
+// });
 
 router.get("/getLoc", (req, res) => {
 	URL = "https://www.refugerestrooms.org/api/v1/restrooms/by_location?page=1&per_page=10&offset=0&ada=false&unisex=true&lat=" + req.query.lat + "&lng=" + req.query.lng
-	request.get(URL, (err,response, body) => {
+	request.get(URL, (err, response, body) => {
 		res.type("application/json")
 		res.send(JSON.parse(body))
 	})
 })
 
-router.get("/testing", (req, res) =>{
-	db.connect(function(err, connection){
-		db.query('SELECT * FROM `reviews`', function (error, results, fields){
-			console.log(results);
-		});
-	});
-	res.send();
-});
+router.get("/getImg", (req, res) => {
+	URL = "https://www.flickr.com/services/rest/?method=flickr.photos.search&api_key=" + flickrKey.key + "&lat=" + req.query.lat + "&lon=" + req.query.lng + "&radius=0.01&page=1&format=json&nojsoncallback=1"
+	request.get(URL, (err, response, body) => {
+		res.type("application/json")
+		res.send(JSON.parse(body).photos.photo)
+	})
+})
+
+// router.get("/testing", (req, res) =>{
+// 	db.connect(function(err, connection){
+// 		db.query('SELECT * FROM `reviews`', function (error, results, fields){
+// 			console.log(results);
+// 		});
+// 	});
+// 	res.send();
+// });
 
 module.exports = router;

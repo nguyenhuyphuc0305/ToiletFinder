c333bc59a4c727e50e3a2b3cb4a5ac2a33234099
Alex Ye <14alexye00@gmail.com>
2020-03-14 18:49:02 -0400

    fixing up merge

--- public/js/map.js
+++ public/js/map.js
@@ -3,17 +3,6 @@ var com = document.getElementById("comments");
 let lat, lng;
 let data;
 
-let mymap = L.map('mapid').setView([51.505, -0.09], 16);
-
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'pk.eyJ1IjoiY2h1Y3VueGFuaDJrIiwiYSI6ImNrNzJ0YmdjcjA1dmozZ24xbjduMWcxNWYifQ.FQ24h62XknEmmVXQ_9k6gg'
-}).addTo(mymap);
-
 function chooseMarker(data) {
     selectedID = data.id
     document.getElementById("location-name").innerHTML = data.name

6622855ac3cf879e16d4457fba99234d1c247727
huyphuc0305 <huyphuc.nguyen0305@gmail.com>
2020-03-14 17:21:05 -0400

    fixed view port, added initialized marker, fix flickr img

--- public/js/map.js
+++ public/js/map.js
@@ -8,17 +8,6 @@ function loaded(){
     getLocation();
 }
 
-let mymap = L.map('mapid').setView([51.505, -0.09], 16);
-
-L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
-    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-    maxZoom: 18,
-    id: 'mapbox/streets-v11',
-    tileSize: 512,
-    zoomOffset: -1,
-    accessToken: 'pk.eyJ1IjoiY2h1Y3VueGFuaDJrIiwiYSI6ImNrNzJ0YmdjcjA1dmozZ24xbjduMWcxNWYifQ.FQ24h62XknEmmVXQ_9k6gg'
-}).addTo(mymap);
-
 function chooseMarker(data) {
     selectedID = data.id
     document.getElementById("location-name").innerHTML = data.name
@@ -29,8 +18,8 @@ function chooseMarker(data) {
         if (xhr.readyState == 4 && xhr.status == 200) {
             let imgSet = JSON.parse(xhr.responseText)
             let img = document.getElementById("location-img")
-            img.crossOrigin = ""
-            img.src = "https://www.flickr.com/photos/" + imgSet[0].owner + "/" + imgSet[0].id
+            let randomNum = Math.floor(Math.random() * imgSet.length)
+            img.src = "https://farm" + imgSet[randomNum].farm + ".staticflickr.com/" + imgSet[randomNum].server + "/" + imgSet[randomNum].id + "_" + imgSet[randomNum].secret + ".jpg"
         }
     }
 
@@ -48,8 +37,27 @@ function getLocation(){
     navigator.geolocation.getCurrentPosition((position) => {
         lat = position.coords.latitude;
         lng = position.coords.longitude;
+        let mymap = L.map('mapid').setView([lat, lng], 16);
 
-        mymap.panTo(new L.LatLng(lat, lng));
+        let mainLocationIcon = new L.Icon({
+            iconUrl: 'https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png',
+            shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+            iconSize: [25, 41],
+            iconAnchor: [12, 41],
+            popupAnchor: [1, -34],
+            shadowSize: [41, 41]
+        });
+
+        L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+            attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+            maxZoom: 18,
+            id: 'mapbox/streets-v11',
+            tileSize: 512,
+            zoomOffset: -1,
+            accessToken: 'pk.eyJ1IjoiY2h1Y3VueGFuaDJrIiwiYSI6ImNrNzJ0YmdjcjA1dmozZ24xbjduMWcxNWYifQ.FQ24h62XknEmmVXQ_9k6gg'
+        }).addTo(mymap);
+
+        L.marker([lat, lng], {icon: mainLocationIcon}).addTo(mymap)
 
         let xhr = new XMLHttpRequest()
         xhr.onreadystatechange = () => {

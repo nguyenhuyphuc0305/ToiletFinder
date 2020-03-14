fb6bc90953fdb9f7e36dcf2f366f71d53aabb33e
notoslo <32404065+notoslo@users.noreply.github.com>
2020-03-14 18:35:17 -0400

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -6,18 +6,18 @@ const request = require("request")
 const router = express.Router()
 const flickrKey = require("../flickr_API.json")
 
-// const mysql = require('mysql');
-// const db = mysql.createConnection({
-// 	host: 'localhost',
-// 	user: 'root',
-// 	password: 'password',
-// 	database: 'toiletdb'
-// });
+const mysql = require('mysql');
+const db = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: 'password',
+	database: 'toiletdb'
+});
 
-// db.connect((err) => {
-// 	if (err) throw err;
-// 	console.log('Connected to toiletdb')
-// });
+db.connect((err) => {
+	if (err) throw err;
+	console.log('Connected to toiletdb')
+});
 
 router.get("/getLoc", (req, res) => {
 	URL = "https://www.refugerestrooms.org/api/v1/restrooms/by_location?page=1&per_page=10&offset=0&ada=false&unisex=true&lat=" + req.query.lat + "&lng=" + req.query.lng
@@ -34,15 +34,6 @@ router.get("/getImg", (req, res) => {
 		res.send(JSON.parse(body).photos.photo)
 	})
 })
-
-// router.get("/testing", (req, res) =>{
-// 	db.connect(function(err, connection){
-// 		db.query('SELECT * FROM `reviews`', function (error, results, fields){
-// 			console.log(results);
-// 		});
-// 	});
-// 	res.send();
-// });
 
 router.post("/review", (req, res) =>{
 	var id = req.query.id
